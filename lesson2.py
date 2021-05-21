5d51945a7c5890e60ead28a864b8d5b694f7e2f2
dmytro <dzhigaev@gmail.com>
2021-05-21 20:27:06 +0300

    adding homework for 2,3 and 4 lesson

--- lesson2.py
+++ lesson2.py
@@ -2,4 +2,5 @@ import this
 from keyword import kwlist
 
 print("\n")
+print("\n")
 print("Keywords are:\n", kwlist)
--- lesson3.py
+++ lesson3.py
@@ -1,2 +1,2 @@
-for i in range(40):
+for i in range(5):
     print(i, " changes to file")
