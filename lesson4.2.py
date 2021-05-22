44d3f887b188cfd98ea4ff5d10bf6a3981849f7c
dzhigaev <84348092+dzhigaev@users.noreply.github.com>
2021-05-22 14:26:10 +0300

    Delete lesson4.2.py

--- lesson4.2.py
+++
@@ -1,4 +0,0 @@
-user_input = float(input("please enter any digit"))
-input_to_str, input_to_int = str(user_input), int(user_input)
-print("input_to_str ", input_to_str)
-print("input_to_int ", input_to_int)
