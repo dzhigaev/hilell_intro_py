5683911436a4a4ad07224091533930e3c79e4001
dzhigaev <84348092+dzhigaev@users.noreply.github.com>
2021-06-22 14:16:30 -0400

    Deleted unnecessary comments

--- lesson10.py
+++ lesson10.py
@@ -3,14 +3,11 @@
 # [{'color': 'red', 'value': 'high'}, {'color': 'yellow', 'value': 'low'}]
 # создать список кортежей
 # [('red', 'high'), ('yellow', 'low')]
-
 colors = [{'color': 'red', 'value': 'high'}, {'color': 'yellow', 'value': 'low'}]
-# new_list = [val for dict_ in colors for val in dict_.values()]
 new_list1 = [tuple(dict_.values()) for dict_ in colors]
 print(new_list1)
 
 # Задача 2
-#
 # Дано словарь
 # a_dictionary = {"a": 1, "b": 2, "c": 3}
 # преобразовать его в список кортежей
@@ -21,12 +18,10 @@ print(my_list_of_tuples)
 
 
 # Задача 3
-#
 # Дано два списка
 # list_a = [1, 2, 3, 4]
 # list_b = [5, 6, 7, 8]
 # Создать из них список кортежей
-#
 # list_c = [(1,5), (2,6), (3,7), (4,8)]
 list_a = [1, 2, 3, 4]
 list_b = [5, 6, 7, 8]
@@ -34,7 +29,6 @@ list_c = list(zip(list_a, list_b))
 print(list_c)
 
 # Задача 4
-#
 # Дано словарь
 # {1:'entry 1', 2:'entry 2', 3:'entry 3', 4:'entry 4'}
 # Создать кортеж занчений для первих трьох єлементов словоря
@@ -43,7 +37,6 @@ my_tuple = tuple(list(user_entry.values())[0:3])
 print(my_tuple)
 
 # Задача 5
-#
 # Дано список
 # ["bar", "baz", "qux", "etc"]
 # Создать кортеж
